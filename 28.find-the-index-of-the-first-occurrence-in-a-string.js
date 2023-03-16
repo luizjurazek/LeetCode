b0027abc42e7953812601377775c6412bdc9b707
Luiz Jurazek <luizjurazek@gmail.com>
2023-03-16 19:41:14 -0300

    Exercicios 21 e 28

---
+++ 28.find-the-index-of-the-first-occurrence-in-a-string.js
@@ -0,0 +1,62 @@
+/*
+ * @lc app=leetcode id=28 lang=javascript
+ *
+ * [28] Find the Index of the First Occurrence in a String
+ *
+ * https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string/description/
+ *
+ * algorithms
+ * Easy (38.87%)
+ * Likes:    2964
+ * Dislikes: 144
+ * Total Accepted:    1.6M
+ * Total Submissions: 4.2M
+ * Testcase Example:  '"sadbutsad"\n"sad"'
+ *
+ * Given two strings needle and haystack, return the index of the first
+ * occurrence of needle in haystack, or -1 if needle is not part of
+ * haystack.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: haystack = "sadbutsad", needle = "sad"
+ * Output: 0
+ * Explanation: "sad" occurs at index 0 and 6.
+ * The first occurrence is at index 0, so we return 0.
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: haystack = "leetcode", needle = "leeto"
+ * Output: -1
+ * Explanation: "leeto" did not occur in "leetcode", so we return -1.
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * 1 <= haystack.length, needle.length <= 10^4
+ * haystack and needle consist of only lowercase English characters.
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+ var strStr = function(haystack, needle) {
+    let stringA = haystack;
+    let stringB = needle;
+    let indexOffirst = stringA.indexOf(stringB)
+
+    return indexOffirst;
+};
+// @lc code=end
+
--- rascunho.js
+++ rascunho.js
@@ -1,16 +1,10 @@
-let a = "11"
-let b = "1"
-var addBinary = function(a, b) {
-    // Convertendo as strings em números da base decimal
-    let decimalA = parseInt(a, 2)
-    let decimaB = parseInt(b, 2)
+let a = "sadbutsad"
+let b = "jdiujd"
+var strStr = function(haystack, needle) {
+    let stringA = haystack;
+    let stringB = needle;
+    let indexOffirst = stringA.indexOf(stringB)
 
-    // soma dos decimais
-    let sum = decimalA + decimalB;
-
-    // Converte o resultado de volta para binario 
-    let binarySum = sum.toString(2);
-
-    return binarySum;
+    return indexOffirst;
 }; 
-addBinary(a, b)
+strStr(a, b)
